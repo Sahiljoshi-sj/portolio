15abb6e57f95789b7a43cd225de920c3ad5b3751
chandra shekhar <asusxe512@gmail.com>
2025-03-19 15:36:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#element', {
+        strings: ['"Web Developer"', '"Graphic Designer"', '"Content Writer"', '"Nature Enthusiast"'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 1000,
+        startDelay: 500,
+        loop: true
+    });
+    new Typed("#element", options);
+});
+
